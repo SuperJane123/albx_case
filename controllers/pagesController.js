2d92d1259f8610a3af12f9c5f72bace016ff1436
943161238@qqcom <943161238@qq.com>
2019-08-04 22:55:51 +0800

    04-后台页面搭建完成plus

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -21,3 +21,82 @@ exports.detailHtml = (req,res)=>{
     res.render('detail');
 };
 
+
+
+
+
+
+
+// ---------后端页面处理区域-----------
+
+// 1.负责处理后台admin里的index页面
+exports.adminIndexHtml = (req,res)=>{
+    res.render('admin/index');
+};
+
+
+2.// 2.处理后台categories页面
+
+exports.categoriesHtml = (req,res)=>{
+    res.render('admin/categories');
+};
+
+
+// 3.处理后台categories页面
+exports.loginHtml = (req,res)=>{
+    res.render('admin/login');
+};
+
+
+// 4.处理后台nav-menus页面
+exports.navMenusHtml = (req,res)=>{
+    res.render('admin/nav-menus');
+};
+
+// 5.处理后台password-reset页面
+exports.passwordResetHtml = (req,res)=>{
+    res.render('admin/password-reset');
+};
+
+// 6.处理后台post-addt页面
+exports.postAddtHtml = (req,res)=>{
+    res.render('admin/post-add');
+};
+
+
+
+// 7.处理后台post-addt页面
+exports.postsHtml = (req,res)=>{
+    res.render('admin/posts');
+};
+
+// 8.处理后台post-addt页面
+exports.profileHtml = (req,res)=>{
+    res.render('admin/profile');
+};
+
+
+// 9.处理后台settings页面
+exports.settingsHtml = (req,res)=>{
+    res.render('admin/settings');
+};
+
+
+// 10.处理后台settings页面
+exports.slidesHtml = (req,res)=>{
+    res.render('admin/slides');
+};
+
+
+// 11.处理后台users页面
+exports.usersHtml = (req,res)=>{
+    res.render('admin/users');
+};
+
+
+
+// 12.处理后台comments页面
+exports.commentsHtml = (req,res)=>{
+    res.render('admin/comments');
+
+};
--- router.js
+++ router.js
@@ -19,4 +19,62 @@ router.get('/',pagesController.indexHtml)
 
 
 
+
+
+
+
+
+// ---------后端页面处理区域-----------
+
+// 1.处理后台index页面
+.get('/admin',pagesController.adminIndexHtml)
+
+
+// 2.处理后台categories页面
+.get('/admin/categories',pagesController.categoriesHtml)
+
+
+// 3.处理后台login页面
+.get('/admin/login',pagesController.loginHtml)
+
+
+// 4.处理后台nav-menus页面
+.get('/admin/nav-menus',pagesController.navMenusHtml)
+
+// 5.处理后台password-reset页面
+.get('/admin/password-reset',pagesController.passwordResetHtml)
+
+
+// 6.处理后台post-addt页面
+.get('/admin/post-add',pagesController.postAddtHtml)
+
+
+// 7.处理后台post-addt页面
+.get('/admin/posts',pagesController.postsHtml)
+
+
+// 8.处理后台profile页面
+.get('/admin/profile',pagesController.profileHtml)
+
+
+// 9.处理后台settings页面
+.get('/admin/settings',pagesController.settingsHtml)
+
+
+
+// 10.处理后台settings页面
+.get('/admin/slides',pagesController.slidesHtml)
+
+
+// 11.处理后台users页面
+.get('/admin/users',pagesController.usersHtml)
+
+
+// 12.处理后台comments页面
+.get('/admin/comments',pagesController.commentsHtml)
+
+
+
+
+
 module.exports = router;
