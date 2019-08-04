770a8f7f08b71ccdaabdae37a722e948e5f5781f
943161238@qqcom <943161238@qq.com>
2019-08-04 22:36:50 +0800

    03-前台页面搭建完成

--- app.js
+++ app.js
@@ -29,3 +29,6 @@ app.set('views',__dirname+'/views');
 // 引入router来判断不同的请求
 app.use(router)
 
+
+
+
---
+++ router.js
@@ -0,0 +1,18 @@
+// 这个模块主要负责处理判断不同的请求
+// 引入模块区域
+const express = require('express');
+const router = express.Router();
+const pagesController = require('./controller/pagesController');
+
+
+
+// ---------前端页面处理区域-----------
+// 1.处理阿里白秀index页面
+router.get('/',pagesController.indexHtml)
+
+// 2.处理阿里白秀list页面
+.get('/list',pagesController.listHtml)
+
+
+// 3.处理阿里白秀detail页面
+.get('/detail',pagesController.detailHtml)
